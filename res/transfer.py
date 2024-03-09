2f88f0b83535802196423d888eb6a2bed1cf66f3
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-03-10 00:06:02 +0100

    now saving the vehicle to a json file

--- res/transfer.py
+++ res/transfer.py
@@ -3,6 +3,8 @@ for pymunk. It is called once the player presses the start button. It works as f
 
 finding the properties of all parts in the vehicle and rewrite obj.vehicle for pymunk.
 Then this list is used to setup the pymink physics simulation for the vehicle"""
+import os
+import json
 def run(obj):
     print("Ha im not doin' anythin' aight? I'm just yo' placeholder!")
     print("Cya!")
@@ -10,3 +12,10 @@ def run(obj):
     PhysicsVehicle = []
     print("User started with Vehicle: ", obj.Vehicle)
     obj.gm = "game"
+
+    #storing the vehicle as json for readability
+    CurrentPath = os.path.dirname(os.path.realpath(os.path.dirname(__file__)))
+    with open(CurrentPath + "/assets/saves/latest_vehicle.json", "w") as outfile: 
+        json.dump(obj.Vehicle, outfile)
+
+
