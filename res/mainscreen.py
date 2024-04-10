5fad3937c7fabb09179c8a183eca5a5a83ab34b0
JoEragon <mail@johannesfeldmann.de>
2024-04-10 16:18:39 +0200

    nothing
    e e ee eee eeee eeeee eeeeeee eeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeeeee

--- res/mainscreen.py
+++ res/mainscreen.py
@@ -1,15 +1,21 @@
-print("k")
+from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import *
 
-#from tkinter import *
-#from tkinter import Tk
-#print("k")
-#import time
-#print("k")
-#time.sleep(1)
-#def mainscreen():
-    #root = Tk()
-    #root.maxsize(200, 100)
-    #print("finz")
-    #root.mainloop()
 
-#mainscreen()
+def mainscreen():
+    root = Tk()
+    root.title("Project Exoplanet")
+    root.geometry("750x375")
+    img1 = PhotoImage(file="assets/images/wheel.png")
+    stl = ttk.Style(root)
+    stl.element_create('Frame.frame', 'image', img1)
+    frm = ttk.Frame(root, style='Frame.frame')
+    
+    root.mainloop()
+    
+   
+    
+
+mainscreen()
+print("executed ")
